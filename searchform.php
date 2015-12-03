3482d47904e0024673e63af92ce46f452e2bc91f
ensarbaylar <ensarbaylar@yahoo.com.tr>
2015-12-03 10:29:17 +0200

    151203-1

--- searchform.php
+++ searchform.php
@@ -1,7 +1,9 @@
-<div class="input-group peacock-search-bar">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<div class="input-group peacock-search-bar">
 	<span class="input-group-btn">
 		<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
 	</span>
 	<input type="text" class="form-control" placeholder="<?php echo __( 'Search for', 'cleanblog' )?>">
 </div><!-- /input-group -->
 <div class="clearfix"></div>
+</form>
