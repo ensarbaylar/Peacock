cfbaf0e27b8708ec47a7806405728f7c567d7ef5
ensarbaylar <ensarbaylar@yahoo.com.tr>
2015-12-07 10:21:13 +0200

    151207-1

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
-	
 	<!-- Navigation -->
     <nav class="navbar navbar-default navbar-custom navbar-static-top">
         <div class="container-fluid">
