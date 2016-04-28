c1940841f8501b0d20ab86c08b5a59c9ec34f6cf
ensarbaylar <ensarbaylar@yahoo.com.tr>
2016-04-28 21:54:49 +0300

    v0.6

--- functions.php
+++ functions.php
@@ -100,7 +100,7 @@ function peacock_comments_number( $count )
 {
 	if ( !is_admin() ) {
 		global $id;
-		$comments_by_type = separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
+		$comments_by_type = separate_comments( get_comments( array('status' => 'approve', 'post_id' => $id ) ) );
 		return count( $comments_by_type['comment'] );
 	} else {
 		return $count;
